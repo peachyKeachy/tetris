1bd5227995518ec561ce6d78dc4f9e75d69d6f8f
peachyKeachy <156981435+peachyKeachy@users.noreply.github.com>
2024-04-04 03:05:05 -0400

    ShapeJ-2.h

--- ShapeJ-2.h
+++ ShapeJ-2.h
@@ -1,6 +1,6 @@
 //  file: ShapeJ.h
 // class: COP 3003 Programming II
-//    by: Prof. Allen
+//    by: Martin Becerra JR
 //  desc: J shape for game Tetris derived
 //        from the base class Tetromino
 // --------------------------------------------------------
